7045594be1c423ebd5c82e35d108fb95aab47019
Leonardo <leonardo.dolivier@gmail.com>
2022-02-12 13:15:22 -0300

    Added home component html and ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  content?: string;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getPublicContent().subscribe(
+      (data) => {
+        this.content = data;
+      },
+      (err) => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
   }
-
 }
