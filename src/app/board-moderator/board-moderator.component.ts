bac501a6fe95125c0f41a04538edac3d2ddf9a34
Leonardo <leonardo.dolivier@gmail.com>
2022-02-12 13:24:56 -0300

    updated boards for user/moderator/admin, html/ts

--- src/app/board-moderator/board-moderator.component.ts
+++ src/app/board-moderator/board-moderator.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-board-moderator',
   templateUrl: './board-moderator.component.html',
-  styleUrls: ['./board-moderator.component.scss']
+  styleUrls: ['./board-moderator.component.scss'],
 })
 export class BoardModeratorComponent implements OnInit {
+  content?: string;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getModeratorBoard().subscribe(
+      (data) => {
+        this.content = data;
+      },
+      (err) => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
   }
-
 }
