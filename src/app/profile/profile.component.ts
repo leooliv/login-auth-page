98d3964b587a60be5051d9940cb6804e5f2df59a
Leonardo <leonardo.dolivier@gmail.com>
2022-02-12 13:11:09 -0300

    profile html and TS updated

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  currentUser: any;
 
-  constructor() { }
+  constructor(private token: TokenStorageService) {}
 
   ngOnInit(): void {
+    this.currentUser = this.token.getUser();
   }
-
 }
